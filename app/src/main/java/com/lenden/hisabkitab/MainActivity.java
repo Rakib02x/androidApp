e7e3e417ca71717521d3771e1829752989a744bd
Rakib <technicalmedia143@gmail.com>
2026-03-22 00:14:57 +0800

    Initial commit with Android and Web fixes

---
+++ app/src/main/java/com/lenden/hisabkitab/MainActivity.java
@@ -0,0 +1,33 @@
+package com.lenden.hisabkitab;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowCompat;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // ১. সিস্টেম বারের প্যাডিং ঠিক করা
+        View mainView = findViewById(android.R.id.content);
+        ViewCompat.setOnApplyWindowInsetsListener(mainView, (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
+
+        // ২. WebView-তে ক্লিপবোর্ড এবং সিকিউরিটি সেটিংস এনাবল করা
+        WebView webView = this.getBridge().getWebView();
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+    }
+}
